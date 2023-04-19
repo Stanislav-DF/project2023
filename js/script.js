81af1dad60a6fdfc60ecccc2555ede0eb086b5d7
Stas <example@gmail.com>
2023-04-19 21:18:28 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,106 @@
+"use strict";
+
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+const obj = {
+    a:50
+};
+obj.a = 10
+console.log(obj);
+
+{
+    let result = 50;
+    console.log(result);
+}   */
+ ///////////////////////////////// Числа 
+// let number = 4.6;
+
+// console.log(4/0); //infinity
+// console.log('string' * 9 );// Not a number NaN
+
+/////////////////////////////////////Строка 
+// const persone = "Alex";
+
+// const bool = true;// boolean
+
+// console.log(something);
+
+/* let und;
+console.log(und);
+
+////////////////////////Обьекты
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+ console.log(obj.name);
+
+ let arr = ['plun.png', 'orange.jpg', 6, 'apple.bmp', {}]; //Массив
+ console.log(arr[1]); */
+
+//////////////////////// общение с пользователем
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+/* 
+const answer = +prompt("Вам есть 18?", " ");
+console.log(answer + 5);
+ */
+
+/////////////////////////Запись в массив
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', ' ');
+// answers[1] = prompt('Как ваша фамилия?', ' ');
+// answers[2] = prompt('Сколько вам лет?', ' ');
+
+// console.log(typeof(answers)); 
+
+//////////////////////// Интерполяция
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hello ${user}`);
+
+/////////////////////////Опрераторы В JS
+
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+/* let incr = 10,
+    decr = 10;
+
+// incr++; //оператор инкримеита - увеличение на еденицу
+// decr--; // декремент - уменьшение
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 === '8'); */
+/* 
+const   isChecked = false,
+        isClose = false;
+
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 != '6');
+ */
+
+
+
